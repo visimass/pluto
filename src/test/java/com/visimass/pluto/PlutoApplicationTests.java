eac08c3b6189c6ac1c42090277637d97913a052a
Jakob <jake3.rosen@gmail.com>
2020-07-08 21:36:25 -0600

    First addition of project files

---
+++ src/test/java/com/visimass/pluto/PlutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visimass.pluto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
